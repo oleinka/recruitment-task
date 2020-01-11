2f400cd3ab7c64c272bc819031c99bcb09cd7028
Marta <m.olejniczak550@gmail.com>
2020-01-11 22:10:39 +0100

    web simple template

--- src/App.js
+++ src/App.js
@@ -3,15 +3,50 @@ import './App.scss';
 
 const App = () => (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <div>
-
+      <div className="Cart">
+        <h1>Koszyk</h1>
+        <button>Ukryj koszyk</button>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Produkt</th>
+                <th>Nazwa</th>
+                <th>Ilość</th>
+                <th>Cena</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td>Biała sukienka</td>
+                <td></td>
+                <td>102,50 zł</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div>Suma:</div>
       </div>
-      <footer>
-
-      </footer>
+      <div className="shop-wrapper">
+        <div>
+          <h1>Produkty</h1>
+          <select name="sort" id="pet-select">
+            <option value="">Sortuj</option>
+            <option value="by-price">wg ceny</option>
+            <option value="by-name">wg nazwy</option>
+          </select>
+        </div>
+        <div className="shop-items-wrapper">
+          <div>
+            obraz
+            Nazwa
+            cena
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 
