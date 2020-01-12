0da1412c6a8d843de55aeaa8224aae65a8ee2030
Marta <m.olejniczak550@gmail.com>
2020-01-12 11:40:59 +0100

    prepare data of products

--- src/data/products.js
+++ src/data/products.js
@@ -1,9 +1,51 @@
 import dressImage from './assets/img/dress.png';
+import hatImage from './assets/img/hat.png';
+import heelsImage from './assets/img/heels.jpg';
+import pantsImage from './assets/img/pants.jpg'; 
+import shoesImage from './assets/img/shoes.jpg';
+import sweaterStripesImage from './assets/img/sweater-stripes.jpg';
+import sweaterImage from './assets/img/sweater.png';
+import tShirtImage from './assets/img/t-shirt.jpg';
 
 export const products = [
     {
         image: dressImage,
-        name: 'Biała Sukienka',
-        price: '55 zł',
+        name: 'Sukienka',
+        price: 55,
+    },
+    {
+        image: hatImage,
+        name: 'Kapelusz',
+        price: 25,
+    },
+    {
+        image: heelsImage,
+        name: 'Wysokie buty damskie',
+        price: 112,
+    },
+    {
+        image: pantsImage,
+        name: 'Spodnie damskie',
+        price: 85,
+    },
+    {
+        image: shoesImage,
+        name: 'Buty sportowe',
+        price: 120.50,
+    },
+    {
+        image: sweaterStripesImage,
+        name: 'Sweter w paski',
+        price: 50,
+    },
+    {
+        image: sweaterImage,
+        name: 'Sweter męski',
+        price: 50,
+    },
+    {
+        image: tShirtImage,
+        name: 'T-Shirt',
+        price: 25.50,
     },
 ]
