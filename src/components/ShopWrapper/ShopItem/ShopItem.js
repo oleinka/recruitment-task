3fdc7d151304a3d3951dce53301a63378f329c85
Marta <m.olejniczak550@gmail.com>
2020-01-13 20:00:55 +0100

    props refactoring

--- src/components/ShopWrapper/ShopItem/ShopItem.js
+++ src/components/ShopWrapper/ShopItem/ShopItem.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './ShopItem.scss';
 
-const ShopItem = (props) => (
-    <div className="shopItemWrapper">
-        <div className="photoItem"><img src={props.image} alt="obrazek"/></div>
-        <div className="detailsItem">
-            <h3>{props.name}</h3>
-            <span>{props.price} zł</span>
-            <button>Dodaj do koszyka</button>
-        </div>
-    </div>
-);
+const ShopItem = ({
+    image,
+    name,
+    price,
+}) => {
+        return (<div className="shopItemWrapper">
+            <div className="photoItem"><img src={image} alt="obrazek" /></div>
+            <div className="detailsItem">
+                <h3>{name}</h3>
+                <span>{price} zł</span>
+                <button>Dodaj do koszyka</button>
+            </div>
+        </div>);
+    };
 
 export default ShopItem;
