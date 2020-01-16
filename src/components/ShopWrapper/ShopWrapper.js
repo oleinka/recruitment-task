c147a367829f5cb5d1556d5b848e2b4cb7dfd881
Marta <m.olejniczak550@gmail.com>
2020-01-16 23:29:39 +0100

    sort not finished

--- src/components/ShopWrapper/ShopWrapper.js
+++ src/components/ShopWrapper/ShopWrapper.js
@@ -3,23 +3,30 @@ import './ShopWrapper.scss';
 import ShopItem from './ShopItem/ShopItem';
 import { products } from '../../data/products';
 
-const ShopWrapper = () => (
+const ShopWrapper = () => {
+  
+  let productItems = products;
+
+  
+
+return (
     <div className="shopWrapper">
         <div className="shopHeading">
           <h1>Produkty</h1>
           <select name="sort">
             <option value="">Sortuj</option>
             <option value="by-price">wg ceny</option>
-            <option value="by-name">wg nazwy</option>
+            <option value="by-name"/* onClick={productItems.sort((a,b)=>a.name-b.name)} */>wg nazwy</option>
           </select>
         </div>
         <div className="shopItemsWrapper">
-          {products.map(item => (
+          {productItems.map(item => (
             <ShopItem key={item.name} {...item}
           />
           ))}
         </div>
     </div>
 );
+}
 
 export default ShopWrapper; 
