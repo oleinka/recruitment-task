f51d02f604af0221acbaa0547497700edbb00d28
Marta <m.olejniczak550@gmail.com>
2020-01-16 23:46:37 +0100

    onChange value

--- src/components/CartWrapper/CartItem/CartItem.js
+++ src/components/CartWrapper/CartItem/CartItem.js
@@ -6,13 +6,19 @@ const CartItem = ({
     name,
     price,
 }) => {
+    
+    const handleChange = (event) => {
+        const value = event.target.value;
+        console.log(value);
+    }
+    
     return (
     <tr className="cartItemWrapper">
         <td>{image}</td>
         <td>{name}</td>
         <td>
             <div>
-                <input type='number' min='0'/>
+                <input type='number' min='0' onChange={handleChange}/>
             </div>
         </td>
         <td>{price}</td>
